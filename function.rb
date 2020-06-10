d0b2a8fc2f8b6c86e78697ea06f5feb40cf64d97
Jamie Macey <jamie.git@tracefunc.com>
2020-06-09 20:07:47 -0700

    wire up jira version lookup

--- function.rb
+++ function.rb
@@ -5,6 +5,26 @@ require 'pp'
 require 'slack-ruby-client'
 require 'jira-ruby'
 
+def get_jira_release(matching)
+  options = {
+    username: ENV['JIRA_USER'],
+    password: ENV['JIRA_TOKEN'],
+    site: 'https://versapayclientservices.atlassian.net',
+    context_path: '',
+    auth_type: :basic,
+    read_timeout: 120,
+  }
+
+  client = JIRA::Client.new(options)
+  current_release = client
+    .Project
+    .find('VA')
+    .versions
+    .select{|v| !v.released && v.name =~ matching }
+    .sort_by(&:releaseDate)
+    .first
+end
+
 def set_slack_topic(channel, topic)
   Slack.configure do |config|
     config.token = ENV['SLACK_OAUTH_TOKEN']
@@ -21,5 +41,7 @@ def set_slack_topic(channel, topic)
   )
 end
 
-SLACK_CHANNEL='bot-testing'
-# set_slack_topic(SLACK_CHANNEL, "Testing From Script")
+release = get_jira_release(/Maintenance/)
+orig_topic = "This group is responsible for ARC maintenance. :stuck_out_tongue: "
+topic = "Current Release: #{release.name} targeting #{release.userReleaseDate}"
+set_slack_topic('bot-testing', orig_topic + topic)
