546342a647239c2d7fbac2ed66b1b573ffd402ed
Jamie Macey <jamie.macey@versapay.com>
2023-06-26 10:13:38 -0700

    tweak payload to avoid slack truncating it

--- lambda_function.rb
+++ lambda_function.rb
@@ -45,12 +45,22 @@ def set_slack_topic(channel, topic)
 end
 
 def topic_from(releases)
+  # Note: Don't use unicode, Slack will convert to :emoji: representation internally and truncate.
+  # Hard cap is supposed to be 250 chars, but the remote API truncates around 239
+  # so we arrange our payload to hover under that. Currently 3 releases is 229.
   "C-AR Maintenance schedule (hover me)\n" +
   releases.map { |release|
     deploy_date = Date.parse(release.releaseDate)
     verify_date = deploy_date - 2
-    ":construction: #{release.name} - verify by EOD #{verify_date.strftime("%b. %-d")}, deploy #{deploy_date.strftime("%b. %-d")}"
+    freeze_date = deploy_date - 6
+    release_name = release.name.gsub(/CAR /,"")[0...20]
+
+    ":ship:#{date_fmt(deploy_date)} :gh-green:#{date_fmt(verify_date)} :ice_cube:#{date_fmt(freeze_date)} >#{release_name}"
   }.join("\n")
+end
+
+def date_fmt(date)
+  date.strftime("%b%-d")
 end
 
 def lambda_handler(*)
